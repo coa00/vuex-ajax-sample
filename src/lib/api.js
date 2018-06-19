106cfb753a9963fec3c2c373faa45f15b43a9723
kouhei aoki <coa@rokuro.net>
2018-06-19 11:29:13 +0900

    getter のサンプルを追加、説明コメントを追加

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,12 @@
 import fetch from "cross-fetch";
 import types from "./apiTypes";
 
-//同じようなコードが続くようであれば types をループで回しても良い。
+// api の処理はここに集約
+// api vuex を使わない場合は、api モジュールは直接呼ぶこと。ただし実装は合わせること。
+
+// Fetch の使い方 https://developer.mozilla.org/ja/docs/Web/API/Fetch_API
+// cross-fetch は、node, ブラウザの両方で動作するfetch
+// https://github.com/lquixada/cross-fetch
 const api = {
   [types.TEST]: data => {
     return fetch("https://api.github.com/users/coa00/repos", {
@@ -12,12 +17,15 @@ const api = {
         if (res.status >= 400) {
           throw new Error("Bad response from server");
         }
+        //jsonを返す。
+        //データの加工が必要ば場合は、ここで加工する
         return res.json();
       })
       .catch(err => {
         throw err;
       });
   },
+  // 失敗パターン
   [types.FAIL]: data => {
     return fetch("https://fail", {
       method: "GET",
--- src/lib/store.js
+++ src/lib/store.js
@@ -7,6 +7,9 @@ import types from "./apiTypes";
 
 Vue.use(Vuex);
 
+//apiの処理とそれ以外にデータ連携が必要なものだけ vuex を使うこと。
+
+// state の初期値
 const state = {
   isLoading: false,
   error: ""
@@ -21,36 +24,54 @@ const mutations = {
   }
 };
 
+// getters データを関数などでフィルタリングするときのみ使いしょう。
+const getters = {
+  getTestById: state => id => {
+    return state[types.TEST].find(item => item.id === id);
+  }
+};
+
 const actions = {};
 
 // api の state, mutation, action をまとめて登録
 _.each(types, type => {
   state[type] = {};
+
   mutations[type] = function(state, payload) {
+    // state の下にタイプ名をキーにしてAPIモジュールから取得したデータを格納
     state[type] = { ...state[type], ...payload };
     return state;
   };
 
+  //Promise.all などが使えるように Promise を戻り値とする
   actions[type] = function({ commit }) {
-    commit("loading", true);
-    api[type]()
-      .then(res => {
-        commit("loading", false);
-        commit(type, res);
-      })
-      .catch(error => {
-        log.error(error);
-        commit("loading", false);
-        commit("error", "通信に失敗しました");
-      });
+    return new Promise((resolve, reject) => {
+      commit("loading", true);
+      api[type]()
+        .then(res => {
+          commit("loading", false);
+          commit(type, res);
+          resolve();
+        })
+        .catch(error => {
+          //エラーパターン サーバからメッセージがあれば表示。
+          commit("loading", false);
+          commit("error", error.message || "通信に失敗しました");
+          log.error(error);
+          reject(error);
+          throw error;
+        });
+    });
   };
 });
 
 const store = new Vuex.Store({
+  // 厳格モード エラーが厳しくします。プロダクションの時は外すこと
   strict: process.env.NODE_ENV !== "production",
   state,
   mutations,
-  actions
+  actions,
+  getters
 });
 
 export default store;
